2accca73d0e8a075c6dd02f56d3ef6698d33d4b7
ivanilsonsoares <ivanilson.soares.is@gmail.com>
2021-03-02 23:39:46 -0300

    Atividade 5 - inserindo elementos

--- Atividades/Atividade5/js/global.js
+++ Atividades/Atividade5/js/global.js
@@ -25,25 +25,22 @@ openModal.addEventListener("click", function () {
 
 buttonClose.addEventListener("click", function () {
     overlayModal.classList.remove("active");
-    location.reload();
+    nome_card.value = '';
+    mensagem_card.value = '';
 })
 
 modalClose.addEventListener("click", function () {
     overlayModal.classList.remove("active");
-    location.reload();
+    nome_card.value = '';
+    mensagem_card.value = '';
 })
 
-buttonSave.addEventListener("click", function () {
-    alert("Mensagem enviada com sucesso!!!");
-    overlayModal.classList.remove("active");
-    location.reload();
-
-})
 
 window.addEventListener("click", function (event) {
     if (event.target == overlayModal) {
         overlayModal.classList.remove("active");
-        location.reload();
+        nome_card.value = '';
+        mensagem_card.value = '';
     }
 })
 
@@ -63,21 +60,21 @@ function number(minimo, maximo) {
     minimo = 0;
     maximo = 2;
     return Math.floor(Math.random() * (maximo - minimo)) + minimo;
-  }
+}
 
-  let post = 0;
-  let Old;
+let post = 0;
+let Old;
 
-  let Interval = setInterval(function() {
+let Interval = setInterval(function () {
 
     let equal = true
 
     while (equal) {
-      post = number(0, posts_data.length)
+        post = number(0, posts_data.length)
 
-      if(post !==Old) {
-        equal = false;
-      }
+        if (post !== Old) {
+            equal = false;
+        }
 
     }
 
@@ -86,11 +83,49 @@ function number(minimo, maximo) {
     let mudarNome = document.querySelector(".info .mudar-nome")
     let mudarInfo = document.querySelector(".info .mudar-info")
 
-    let nome = mudarNome.innerHTML = posts_data[post].nome
-    let info = mudarInfo.innerHTML = posts_data[post].mensagem
+    nome = mudarNome.innerHTML = posts_data[post].nome
+    info = mudarInfo.innerHTML = posts_data[post].mensagem
 
-    h3.appendChild(nome);
-    h1.appendChild(info);
+}, 2000)
 
-  }, 2000)
 
+let nome_card = document.querySelector(".nome-form");
+let mensagem_card = document.querySelector(".mensagem-form");
+
+let submit_button = document.querySelector(".submit-button")
+
+let mural = document.querySelector(".container");
+
+submit_button.addEventListener("click", function (event) {
+    event.preventDefault();
+    let nome = nome_card.value;
+    let mensagem = mensagem_card.value;
+
+    let post = document.createElement("div");
+    post.classList.add("info");
+    mural.appendChild(post);
+
+    let h3 = document.createElement("h3");
+    h3.classList.add("info-nome");
+    let text_h3 = document.createTextNode(nome);
+
+    let img = document.createElement("img");
+    img.classList.add("avatar");
+
+    h3.appendChild(img);
+    h3.appendChild(text_h3);
+    post.appendChild(h3);
+
+    let h1 = document.createElement("h1");
+    h1.classList.add("info-mensagem");
+    let text_h1 = document.createTextNode(mensagem);
+
+    h1.appendChild(text_h1);
+    post.appendChild(h1);
+
+    overlayModal.classList.remove("active");
+
+    nome_card.value = '';
+    mensagem_card.value = '';
+
+})
